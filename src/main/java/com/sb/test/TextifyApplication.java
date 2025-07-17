5d91d855598a047924add5d25c25b20c5d447347
Megha Vivek Gaonkar <meghavivekgaonkar@gmail.com>
2025-07-17 12:06:16 -0700

    initial

---
+++ src/main/java/com/sb/test/TextifyApplication.java
@@ -0,0 +1,13 @@
+package com.sb.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextifyApplication.class, args);
+	}
+
+}
